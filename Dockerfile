a4fd48f1db27caad25f1c45b8f5b5cf9e1f31337
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2020-05-02 00:54:39 +0200

    test build react docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM node:12 AS builder
+FROM node:12
 
 RUN mkdir /app
 WORKDIR /app
 
 COPY package*.json ./
 RUN npm ci --only=production
-
+RUN npm build
 COPY . .
 
 CMD [ "npm", "run","start" ]
--- src/react/index.js
+++ src/react/index.js
@@ -1,10 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
-class App extends Component {
-    render() {
-        return <div>This is a React component inside of Webflow!</div>;
-    }
-}
+import { decode } from 'blurhash';
+
+const App = () => {
+    const [hash, hashSet] = useState('');
+    const [imageUrl, imageUrlSet] = useState('');
+    const canvas = useRef(null);
+
+    const getHash = (e) => {
+        e.preventDefault();
+        const imageURL = document.getElementById('imageUrl').value;
+        imageUrlSet(imageURL);
+        fetch('/blur', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ imageURL }),
+        })
+            .then((x) => x.json())
+            .then((y) => {
+                hashSet(y.hash);
+                const pixels = decode(y.hash, 250, 250);
+                canvas.current.width = 250;
+                canvas.current.height = 250;
+                const ctx = canvas.current.getContext('2d');
+                const imageData = ctx.createImageData(250, 250);
+                imageData.data.set(pixels);
+                ctx.putImageData(imageData, 0, 0);
+            });
+    };
+    return (
+        <div>
+            <p>lorem</p>
+            <form onSubmit={getHash}>
+                <div class="row">
+                    <div class="six columns">
+                        <label for="imageUrl">URL</label>
+                        <input
+                            type="text"
+                            required
+                            class="u-full-width"
+                            placeholder="Fill in the URL of an image"
+                            name="imageUrl"
+                            id="imageUrl"
+                        />
+                    </div>
+                </div>
+                <input
+                    class="button-primary"
+                    type="submit"
+                    value="Hash it up!"
+                />
+            </form>
+            {hash && (
+                <code style={{ fontSize: '2rem', marginBottom: '2rem' }}>
+                    {hash}
+                </code>
+            )}
+            {hash && imageUrl && <img src={imageUrl}></img>}
+            <canvas ref={canvas}></canvas>
+        </div>
+    );
+};
 
 ReactDOM.render(
     React.createElement(App, {}, null),
