62686937ab39dd7cd3de60fd1caa8766cda4420c
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2020-05-04 01:20:45 +0200

    add loom

--- src/react/index.js
+++ src/react/index.js
@@ -69,6 +69,16 @@ const App = () => {
                 <img onLoad={loadedImage} src={imageUrl}></img>
                 <canvas ref={canvas}></canvas>
             </div>
+            <div style="position: relative; padding-bottom: 56.25%; height: 0;">
+                <iframe
+                    src="https://www.loom.com/embed/a2787e0276c84100928408bdaa1709e5"
+                    frameborder="0"
+                    webkitallowfullscreen
+                    mozallowfullscreen
+                    allowfullscreen
+                    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
+                ></iframe>
+            </div>
         </div>
     );
 };
